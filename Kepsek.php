d3c8a7425ece63dfb7160f08401f25aa8f9a5d2c
R13ka <144298652+R13ka@users.noreply.github.com>
2023-10-07 19:11:07 +0700

    Update Kepsek.php

--- Kepsek.php
+++ Kepsek.php
@@ -1,1 +1,27 @@
+<html>
+<head>
+   <title>Halaman Kepsek</title>
+</head>
+<body>
+   <?
+   session_start();
 
+   // cek apakah yang mengakses halaman ini sudah login
+if($_SESSION['level']==""){	
+   header("location:index.php? pesan=gagal");
+}
+
+?>
+<h1>halaman kepsek</h1>
+
+<p>Halo <b><?php  echo $_SESSION['username']; ?><b> Anda telah login sebagai <b>
+<? echo $_SESSION['level']; ?></b>.</p>
+<a href="logout.php">LOGOUT</a>
+
+<br/>
+<br/>
+
+<a>Membuat Login Multi Level Dengan PHP</a>
+
+</body>
+</html>
